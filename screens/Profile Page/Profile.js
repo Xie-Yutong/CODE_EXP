d5db69a3a147905f423408ee83ea3119b077a966
Tan Zi Xuan <zixuanvtzx@gmail.com>
2022-06-15 04:44:32 +0800

    zx

--- screens/Profile Page/Profile.js
+++ screens/Profile Page/Profile.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, Image, StyleSheet } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,11 +7,20 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 export default function ProfileScreen({ navigation }) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Image style={styles.soldier} source={require('../../assets/soldier.png')}></Image>
         <Text>Profile screen</Text>
-        <Button
+
+        <Button color="#558f6c"
           title="Go to Details"
           onPress={() => navigation.navigate('Details')}
         />
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    soldier: {
+      width: 100,
+      height: 100,
+    },
+  })
