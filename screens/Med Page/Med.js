76bf21072e347575754e19ec10af6a4ab1ee6874
Tan Zi Xuan <zixuanvtzx@gmail.com>
2022-06-15 02:32:51 +0800

    medcentre ok

--- screens/Med Page/Med.js
+++ screens/Med Page/Med.js
@@ -58,14 +58,19 @@ function SubmissionScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <View style={styles.submissionTop}>
-        <Image style={styles.subImage} source={require('../../assets/tick-solid.png')}></Image>
+        <Image style={styles.subImage} source={require('../../assets/check.png')}></Image>
         <Text style={styles.subTYText}>Thank you for completing the checklist!</Text>
+        <Text style={styles.subNoteText}>Note: You may view your QNo & E.W.T from your Home page.</Text>
       </View>
 
       <View style={styles.submissionBottom}>
-        <Text style={styles.subHeader}>Your queue number:</Text>
+        <TouchableOpacity style={styles.subHeader}>
+        <Text style={styles.subHeaderText}>Your queue number (QNo) :</Text>
+        </TouchableOpacity>
         <Text style={styles.QNo}>102</Text>
-        <Text style={styles.subHeader}>Estimated waiting time:</Text>
+        <TouchableOpacity style={styles.subHeader}>
+        <Text style={styles.subHeaderText}>Your estimated waiting time (E.W.T) :</Text>
+        </TouchableOpacity>
         <Text style={styles.Ewtime}>20 minutes</Text>
       </View>
       </View>
@@ -115,17 +120,54 @@ const styles = StyleSheet.create({
     top: 9,
     margin: 4,
   },
-
+  submissionTop: {
+    alignItems: 'center',
+    paddingBottom: 30,
+  },
+  subImage: {
+    width: 100,
+    height: 100,
+    paddingTop: 50,
+    paddingBottom: 50,
+    alignItems: 'flex-start',
+  },
+  subTYText: {
+    paddingTop: 20,
+  },
+  subNoteText: {
+    paddingTop: 20,
+    fontSize: 12,
+    fontStyle: 'italic',
+  },
+  subHeader: {
+    backgroundColor: '#558F6C',
+    border: 1,
+    borderRadius: 25,
+    width: 370,
+    height: 46,
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 9,
+    margin: 4,
+  },
+  subHeaderText: {
+    padding: 10,
+    color: "#fff",
+  },
   submissionBottom: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  submissionTop: {
-    alignItems: 'center',
-    justifyContent: 'center',
+  QNo: {
+    fontSize: 80,
+    fontWeight: '800',
   },
-  subImage: {},
-  subHeader: {},
+  Ewtime: {
+    paddingTop: 15,
+    paddingBottom: 20,
+    fontSize: 40,
+    fontWeight: '600',
+  },
 
 
 })
